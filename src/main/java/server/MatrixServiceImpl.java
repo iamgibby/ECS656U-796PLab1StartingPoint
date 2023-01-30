cb8bfb87a359156ae7a343ecacafb5716a8048e1
iamgibby <71736174+iamgibby@users.noreply.github.com>
2023-01-30 11:45:36 +0000

    Update MatrixServiceImpl.java

--- src/main/java/server/MatrixServiceImpl.java
+++ src/main/java/server/MatrixServiceImpl.java
@@ -20,15 +20,15 @@ public class MatrixServiceImpl extends MatrixServiceImplBase
 		reply.onCompleted();
 	}
 	@Override
-    public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
-    {
+    	public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
+    	{
         	System.out.println("Request received from client:\n" + request);
         	int C00=request.getA00()*request.getB00();
 		int C01=request.getA01()*request.getB01();
 		int C10=request.getA10()*request.getB10();
 		int C11=request.getA11()*request.getB11();
-        MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
-        reply.onNext(response);
-        reply.onCompleted();
-    }
+        	MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
+        	reply.onNext(response);
+        	reply.onCompleted();
+    	}
 }
